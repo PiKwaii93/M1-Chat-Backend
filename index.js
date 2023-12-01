9bb0dd091fac27078fde3076485e9bd76024fbc2
Barret-Pierre <pierrealexandrebarret.pro@gmail.com>
2023-12-01 15:18:18 +0100

    Feat : Add connection database

--- index.js
+++ index.js
@@ -1,45 +1,62 @@
-const express = require("express")
-const app = express()
-const cors = require("cors")
-const http = require('http').Server(app);
-const PORT = 4000
-const socketIO = require('socket.io')(http, {
-    cors: {
-        origin: "http://localhost:3000"
-    }
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const http = require("http").Server(app);
+require("dotenv").config();
+const mysql = require("mysql2");
+const PORT = 4000;
+const socketIO = require("socket.io")(http, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
 });
 
-app.use(cors())
-let users = []
+app.use(cors());
+let users = [];
 
-socketIO.on('connection', (socket) => {
-    console.log(`⚡: ${socket.id} user just connected!`)  
-    socket.on("message", data => {
-      socketIO.emit("messageResponse", data)
-    })
+socketIO.on("connection", (socket) => {
+  console.log(`⚡: ${socket.id} user just connected!`);
+  socket.on("message", (data) => {
+    socketIO.emit("messageResponse", data);
+  });
 
-    socket.on("typing", data => (
-      socket.broadcast.emit("typingResponse", data)
-    ))
+  socket.on("typing", (data) => socket.broadcast.emit("typingResponse", data));
 
-    socket.on("newUser", data => {
-      users.push(data)
-      socketIO.emit("newUserResponse", users)
-    })
- 
-    socket.on('disconnect', () => {
-      console.log('🔥: A user disconnected');
-      users = users.filter(user => user.socketID !== socket.id)
-      socketIO.emit("newUserResponse", users)
-      socket.disconnect()
-    });
+  socket.on("newUser", (data) => {
+    users.push(data);
+    socketIO.emit("newUserResponse", users);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("🔥: A user disconnected");
+    users = users.filter((user) => user.socketID !== socket.id);
+    socketIO.emit("newUserResponse", users);
+    socket.disconnect();
+  });
 });
 
-app.get("/api", (req, res) => {
-  res.json({message: "Hello"})
+// create a new MySQL connection
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.PORT || 3306,
 });
 
-   
+// connect to the MySQL database
+connection.connect((error) => {
+  if (error) {
+    console.error("Error connecting to MySQL database:", error);
+  } else {
+    console.log("Connected to MySQL database!");
+  }
+});
+
+app.get("/users", (req, res) => {
+  res.json({ message: "Hello" });
+});
+
 http.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
+  console.log(`Server listening on http://localhost:${PORT}`);
 });
